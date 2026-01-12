043f33f5338e26335ac83ba34ef26cce9a72c32e
jithendra341 <jithendrabathula@gmail.com>
2026-01-12 17:32:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  checkinputs();
+});
+
+function checkinputs(){
+  const usernameValue=username.value.trim();
+  const emailValue=email.value.trim();
+  const passwordValue=password.value.trim();
+  const password2Value=password2.value.trim();
+if (usernameValue === '') {
+  document.getElementsByName('small').innerHTML;
+}else {
+  setSuccessFor(username);
+}
+
+}
